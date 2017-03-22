59a7218c263fe96712af969fdcc980b027c1910f
martinphellwig <martin.hellwig@gmail.com>
2017-03-22 16:32:27 +0000

    Extra filtering in the admin view.

--- glitter_documents/mixins.py
+++ glitter_documents/mixins.py
@@ -3,11 +3,16 @@
 from .models import Document, Category
 
 
+def _sort_key(category):
+    return category.title
+
+
 class DocumentMixin(object):
     model = Document
 
     def get_context_data(self, **kwargs):
         context = super(DocumentMixin, self).get_context_data(**kwargs)
         context['documents_categories'] = True
-        context['categories'] = Category.objects.all()
+        categories = Category.objects.filter(parent_category__isnull=True)
+        context['categories'] = categories
         return context
