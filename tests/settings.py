c69b239e8406f8129101a077a9492a82923dd068
Edvinas Jurevicius <ikonitas@gmail.com>
2017-05-24 14:16:45 +0100

    Add image field for the document

--- tests/settings.py
+++ tests/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'glitter',
+    'glitter.assets',
     'glitter.reminders',
     'glitter_documents',
     'taggit',
